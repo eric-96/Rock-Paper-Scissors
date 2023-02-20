c6e66272ee28c250d21282e6ca30908884f6ba50
Eric <ericpossollo@gmail.com>
2023-02-20 23:31:16 +0000

    checkWin() changed to better practice

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const choices = ['rock', 'paper', 'scissors']
-let playerScore = 0
-let computerScore = 0
+let p1Score = 0
+let p2Score = 0
 
 
 function round(){
     
-    let computerSelection = getComputerChoice();
-    let playerSelection = getPlayerChoice();
-    console.log('Player: ' + playerSelection);
-    console.log('Computer: ' + computerSelection);
+    let p2 = getComputerChoice();
+    let p1 = getPlayerChoice();
+    console.log('Player: ' + p1);
+    console.log('Computer: ' + p2);
     
-    const winner = checkWin(playerSelection, computerSelection);
+    const winner = checkWin(p1, p2);
     console.log(winner);   
     console.log('---------------------------------------------------------');
 }
@@ -28,7 +28,7 @@ function getPlayerChoice(){
         alert('You spelled incorrectly.');
         return getPlayerChoice();
     }
-
+    
     return input;
     
 }
@@ -36,39 +36,32 @@ function getPlayerChoice(){
 
 function validateChoice(choice){
     if(choices.includes(choice)){
-         return true}
-    else{ 
-        return false
-    }
-} 
-
-
-function checkWin(playerSelection, computerSelection){
-    if(playerSelection === computerSelection){
-        return 'It\'s a tie!'
-    }
-    else if(
-        (playerSelection === 'rock' && computerSelection === 'paper') ||
-        (playerSelection === 'paper' && computerSelection === 'scissors') ||
-        (playerSelection === 'scissors' && computerSelection === 'rock')
-        )
-        {
-            computerScore++
-            return 'Computer wins'
+        return true}
+        else{ 
+            return false
         }
-        else{
-            playerScore++
-            return 'Player wins'
-    }
-}
-function game(){
-    for (let i = 0; i < 5; i++) {    
+    } 
+    
+    const checkWin = (p1, p2) => {
+        if (p1 === p2) return "Draw!";
+        const rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+        if (p2 === rules[p1]) {
+          return "Player 1 won!";
+        }
+        else {
+          return "Player 2 won!";
+        }
+      };
+    
+    
+    function game(){
+        for (let i = 0; i < 5; i++) {    
     round();
+}
+if (p1Score > p2Score){
+    console.log('You beat the computer!') 
     }
-    if (playerScore > computerScore){
-        console.log('You beat the computer!') 
-    }
-    else if (playerScore === computerScore){
+    else if (p1Score === p2Score){
         console.log('It\'s a tie!')
     }
     else{
